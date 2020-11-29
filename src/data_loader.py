0b30d86fbc3f76fe7c143da6fcd383bec06327ba
Xiangwei Wang <wangxiangwei.cpp@gmail.com>
2020-11-29 23:43:52 +0800

    update part of dataloader: comment

--- src/data_loader.py
+++ src/data_loader.py
@@ -22,25 +22,46 @@ from torch.utils.data.sampler import WeightedRandomSampler
 
 
 def coor_channel(camera_parameter):
-    image = np.zeros((2,camera_parameter[1],camera_parameter[0]),dtype=float)
-    for i_row in range(0,int(camera_parameter[1])):
-        for i_col in range(0,int(camera_parameter[0])):
-            image[0,i_row,i_col] = (i_row - camera_parameter[5])/camera_parameter[3]
-            image[1,i_row,i_col] = (i_col - camera_parameter[4])/camera_parameter[2]
+    """
+    Generate coor channel based on the camera parameters
+    Attributes:
+        camera_parameter: [W, H, F_X, F_Y, C_X, C_Y]
+    Returns:
+        coor_channel: shape [2, H, W]
+    Raise:
+        No
+    """
+    image = np.zeros((2,camera_parameter[1], camera_parameter[0]), dtype=float)
+    for i_row in range(0, int(camera_parameter[1])):
+        for i_col in range(0, int(camera_parameter[0])):
+            image[0, i_row, i_col] = (i_row - camera_parameter[5]) / camera_parameter[3]
+            image[1, i_row, i_col] = (i_col - camera_parameter[4]) / camera_parameter[2]
     return image
+
 class SepeDataset(Dataset):
     """
     Dataset: the dataset can contain multiple sequences, for each sequence a list file
     pose file is necessary, the paths of list files and motion files should be in two txt
     files:
     """
-    def __init__(self, path_to_poses_files, path_to_image_lists, transform_=None,camera_parameter=[640,180,640,640,320,90],norm_flag=0,coor_layer_flag=True,color_flag = True):
+    def __init__(self, path_to_poses_files, path_to_image_lists, 
+                transform_=None, camera_parameter=[640,180,640,640,320,90], 
+                norm_flag=0, coor_layer_flag=True, color_flag = True):
         """
-        Args:
-            motions_file (string): Path to the pose file with camera pose.
-            image_paths_dir (string): Directory with all the images.
+        Initialization functions
+        Attributes:
+            path_to_poses_files (string): Path to the pose file with camera pose.
+            path_to_image_lists (string): Path to image list files
             transform (callable, optional): Optional transform to be applied
                 on a sample.
+            camera_parameter(list):  [W, H, F_X, F_Y, C_X, C_Y]
+            norm_flag (0 or 1): 0 calcualte mean and std; 1 load calculated mean and std
+            coor_layer_flag(bool): use coor layers
+            color_flag (bool)    : use rgb image
+        Return:
+            None
+        Raise:
+
         """
         self.poses_files = pd.read_csv(path_to_poses_files)
         self.image_lists   = pd.read_csv(path_to_image_lists)
@@ -54,9 +75,11 @@ class SepeDataset(Dataset):
         all_ses = []
         self.coor_layer_flag = coor_layer_flag
         self.color_flag = color_flag
+
         if self.coor_layer_flag:
             self.coor_channel = coor_channel(camera_parameter)
             self.coor_channel = torch.from_numpy(self.coor_channel).float()
+
         for i in range(0,self.seq_num):
             poses = np.loadtxt(self.poses_files['data'].loc[i])
             if poses.shape[1] == 7:# quaternion to SE3
@@ -70,32 +93,29 @@ class SepeDataset(Dataset):
             self.motions.append(motions)
             self.image_paths.append(pd.read_csv(self.image_lists['data'].loc[i]))
             self.seq_lengths[i+1] =self.seq_lengths[i]+motions.shape[0]-1
+
         all_ses = all_ses.reshape(int(self.seq_lengths[-1]),6)
         weight = motion_weight(all_ses[:,4])
         samples_weight = torch.from_numpy(weight)
         samples_weigth = samples_weight.double()
-        self.sampler = WeightedRandomSampler(samples_weight,len(samples_weight))
+
+        self.sampler = WeightedRandomSampler(samples_weight, len(samples_weight))
         self.transform = transforms.Compose(transform_)
-        self.motion_means = np.mean(all_ses,0)
-        self.motion_stds  = np.std(all_ses,0)
+        self.motion_means = np.mean(all_ses, 0)
+        self.motion_stds  = np.std(all_ses, 0)
         mean_std_name = path_to_poses_files.split('.')
-        print(mean_std_name)
         mean_std_name = '.'.join(mean_std_name[0:-2])+'.motion_mean_std.txt'
-        print(mean_std_name)
         if norm_flag == 0:
             np.savetxt(mean_std_name,[self.motion_means,self.motion_stds])
         elif norm_flag == 1:
             std_mean = np.loadtxt(mean_std_name)
             self.motion_means = std_mean[0,:]
             self.motion_stds  = std_mean[1,:]
-        print('motion mean and std',self.motion_means,self.motion_stds)
 
     def __len__(self):
         return int(self.seq_lengths[-1])
-        #return 500
 
     def __getitem__(self, idx):
-        #print(idx)
         for i in range(0,self.seq_num):
             if idx<self.seq_lengths[i+1]:
                 #calculate sequence id and data id
@@ -191,6 +211,7 @@ class SepeDataset(Dataset):
                 return sample
             else:
                 continue
+
 
 
 
